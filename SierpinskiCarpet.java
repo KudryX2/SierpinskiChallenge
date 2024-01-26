5a45273a799620532a752abfd4a98f4264df503c
Kudry <15013415+KudryX2@users.noreply.github.com>
2024-01-26 13:59:36 +0100

    Add files via upload

--- SierpinskiCarpet.java
+++ SierpinskiCarpet.java
@@ -3,8 +3,43 @@ public class SierpinskiCarpet {
    public static void main(String[] args) {
         drawSierpinskiCarpet(3, 27);
     }
-    
-    public static void drawSierpinskiCarpet(int order, int size) {
-        //TODO: Implement this 
+
+    private static void drawSierpinskiCarpet(int n, int size) {
+
+        char[][] array = new char[size][size * 2 - 1];
+
+        for(int i = 0 ; i < size - 1 ; ++i){
+            for(int j = 0 ; j < size * 2 - 1 ; ++j){
+                array[i][j] = ' ';
+            }
+        }
+
+        createCarpet(size, array, 0, 0, n);
+
+        for(int i = 0 ; i < size - 1 ; ++i){
+            System.out.println(array[i]);
+        }
+
+    }
+
+    private static void createCarpet(int size, char[][] array, int row, int column, int order) {
+
+        if(size == 1){
+            array[row][column] = '*';
+        }else{
+            int newSize = size/order;
+
+            for(int i = 0 ; i < order ; ++i){
+                for(int j = 0 ; j < order ; ++j){
+
+                    if(i == 1 && j == 1){
+                        continue;
+                    }
+
+                    createCarpet(newSize, array, row + i * newSize, column + j * newSize, order);
+                }
+            }
+        }
+
     }
 }
--- SierpinskiTriangle.java
+++ SierpinskiTriangle.java
@@ -1,14 +1,55 @@
 public class SierpinskiTriangle {
     public static void main(String[] args) {
-        drawTriangle(4)
-
+        drawTriangle(4);
         drawSierpinskiTriangle(3);
     }
     public static void drawTriangle(int height){
-        //TODO: Implement this 
+
+        for(int row = 0 ; row < height - 1 ; ++row){
+            StringBuilder outputLine = new StringBuilder(new String(new char[height * 2]).replace('\0', ' '));
+            if(row == 0){
+                outputLine.replace(height, height+1, "*");
+            }else{
+                outputLine.replace(height-row, height-row+1, "*");
+                outputLine.replace(height+row, height+row+1, "*");
+            }
+
+            System.out.println(outputLine);
+        }
+
+        System.out.println(" " + new String(new char[(height * 2) - 1]).replace('\0', '*'));
+
     };
-    
+
     public static void drawSierpinskiTriangle(int order) {
-     //TODO: Implement this   
+        int size = (int) (Math.pow(2, order + 1) - 1);
+        char[][] array = new char[size][size * 2 - 1];
+
+        for(int i = 0 ; i < size - 1 ; ++i){
+            for(int j = 0 ; j < size * 2 - 1 ; ++j){
+                array[i][j] = ' ';
+            }
+        }
+
+        createTriangle(size, array, 0, size - 1);
+
+        for(int i = 0 ; i < size - 1 ; ++i){
+            System.out.println(array[i]);
+        }
     }
+
+    private static void createTriangle(int size, char[][] array, int row, int column) {
+
+        if(size == 1){
+            array[row][column] = '*';
+        }else{
+            int newSize = size/2;
+
+            createTriangle(newSize, array, row, column);
+            createTriangle(newSize, array, row + newSize, column - newSize);
+            createTriangle(newSize, array, row + newSize, column + newSize);
+        }
+
+    }
+
 }
